1e4b374d8a35b8f2b95d49e8375206a468d89320
Sebastian Avalos <Sebastian Avalos>
2016-06-07 09:08:23 -0500

    Pantalla de Ayuda

---
+++ app/src/main/java/ec/edu/epn/aquariumchecker/views/Ayuda.java
@@ -0,0 +1,23 @@
+package ec.edu.epn.aquariumchecker.views;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import ec.edu.epn.aquariumchecker.R;
+
+public class Ayuda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
