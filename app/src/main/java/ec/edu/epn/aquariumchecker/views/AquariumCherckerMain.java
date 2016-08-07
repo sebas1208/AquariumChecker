7b48194e97290e81e602e6c765e7d7fa0effe241
Nathy <nathy.cumbicos@gmail.com>
2016-08-07 15:13:29 -0500

    Listar Mis Acuarios consumiendo Rest Acuarios

--- app/src/main/java/ec/edu/epn/aquariumchecker/views/AquariumCherckerMain.java
+++ app/src/main/java/ec/edu/epn/aquariumchecker/views/AquariumCherckerMain.java
@@ -13,6 +13,7 @@ public class AquariumCherckerMain extends AppCompatActivity                     
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
     }
 
 
